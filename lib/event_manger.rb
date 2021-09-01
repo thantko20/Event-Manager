42bb5651a2fb39edd76845dc636346e77aeb1cfb
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-01 16:33:36 +0630

    Remove pseudocode

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -9,17 +9,6 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-# Create method for clean phone numbers
-# i.e, If the phone number is less than 10 digits, assume that it is a bad number
-# If the phone number is 10 digits, assume that it is good
-# If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
-# If the phone number is 11 digits and the first number is not 1, then it is a bad number
-# If the phone number is more than 11 digits, assume that it is a bad number
-
-# So I'm thinking of storing numbers as array of string and joining only the digits
-# So how do I omit characters which are not digits?
-# So as per a post on stackoverflow, I can use #delete method with regex like number.delete("^0-9")
-
 def clean_phone_number(number)
   number = number.delete("^0-9").to_s
   length = number.length
@@ -38,18 +27,6 @@ def format_number(number)
   [3, 7].each { |i| number.insert(i, '-')}
   number
 end
-
-# So next assignment is to find out which hours of the most people have registered!
-# Assignment gives me some documentation about Date and Time classes so I'm going to read it first
-# For each row,
-# I will use Time.strptime(regdate, directives) to create a time object with date in csv file
-# I have to create a method for finding the peak hours
-# I will then use #hour method to get hour from Time object
-# I will store each hour as keys in hashes and increment their value if certain key appears
-# I will sort the hashes by using sorting method
-# I can use #sort_by on hash to sort the hours
-# Then I extract only the hours as strings
-# And store them in a file called peakhours.txt?
 
 def get_hour(time)
   time = Time.strptime(time, '%m/%d/%y %H:%M')
@@ -76,15 +53,6 @@ def save_peak_hours(hash, name)
   end
 end
 =end
-
-# I have to find 'days of the week that most people registered'
-# Assignment suggests to use Date#wday method which accepts year, month, day as parameters and
-# return a number which presents day of the week(6 for sunday for exmaple)
-# So I need to extract the date
-# I'll use Date#strptime to extract the date and DateOject#wday to get the day of the week
-# So I'll have to convert the returned number to day of the week
-# Then I'll store the number of days in a hash
-# Then I'll use File.open(filename, 'w') to store data locally
 
 def get_day(time)
   day = Date.strptime(time, '%m/%d/%y %H:%M')
