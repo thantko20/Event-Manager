e2bdab4d0ff4758703255d9dc9e3e107d4739651
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-01 12:03:21 +0630

    Add clean_phone_number method

--- lib/event_manger.rb
+++ lib/event_manger.rb
@@ -6,6 +6,36 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+# Create method for clean phone numbers
+# i.e, If the phone number is less than 10 digits, assume that it is a bad number
+# If the phone number is 10 digits, assume that it is good
+# If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
+# If the phone number is 11 digits and the first number is not 1, then it is a bad number
+# If the phone number is more than 11 digits, assume that it is a bad number
+
+# So I'm thinking of storing numbers as array of string and joining only the digits
+# So how do I omit characters which are not digits?
+# So as per a post on stackoverflow, I can use #delete method with regex like number.delete("^0-9")
+
+def clean_phone_number(number)
+  number = number.delete("^0-9").to_s
+  length = number.length
+
+  if length == 10
+    format_number(number)
+  elsif length == 11 && number[0] == '1'
+    number.slice!(0)
+    format_number(number)
+  else
+    "invalid number"
+  end
+end
+
+def format_number(number)
+  [3, 7].each { |i| number.insert(i, '-')}
+  number
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -46,6 +76,7 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  number = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
